69e0cf3051cf1ab90ffbc2ca3e278da90ffbf84b
Ajayi Lawrence <lawrenceajayi481@gmail.com>
2020-04-12 16:59:45 +0100

    env

--- src/model/user.js
+++ src/model/user.js
@@ -70,7 +70,7 @@ userSchema.methods.toJSON = function() {
 }
 
 userSchema.methods.generateAuthToken = async function() {
-    const token = jwt.sign({_id: this._id.toString()}, 'nodejs')
+    const token = jwt.sign({_id: this._id.toString()}, process.env.JWT_SECRET)
     this.tokens = this.tokens.concat({ token })
     await this.save()
     return token
